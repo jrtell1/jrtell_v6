b6576a6756fa4ed69d88cf8445c7a00eb49f6d44
John <john-robin@jrtell.se>
2019-09-15 18:58:31 +0200

    Corrected og:image

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
       { property: 'og:title', content: 'John-Robin Tell' },
       { property: 'og:description', content: 'Fullstack Developer' },
-      { property: 'og:image', content: 'https://res.cloudinary.com/jrtell/image/upload/w_200,h_200,c_fill,g_face,f_auto/v1552242769/jrtell/Snapchat-1254887129' }
+      { property: 'og:image', content: 'https://res.cloudinary.com/jrtell/image/upload/c_fill,f_auto,g_face,h_184,q_auto,w_184/v1552242867/jrtell/20190310_155108' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
