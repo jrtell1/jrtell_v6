dcfa4c0419067fc8cef9fd16b658c45575818e84
John <john-robin@jrtell.se>
2019-09-15 16:43:31 +0200

    finished navbar

--- components/navigation.vue
+++ components/navigation.vue
@@ -1,20 +1,15 @@
 <template>
   <div class="navigation sticky top-0 overflow-x-auto z-10 -mb-24" :class="dynamicClasses">
     <nav class="container mx-auto flex items-center justify-between py-6 px-4">
-      <div class="flex items-center flex-shrink-0 mr-6">
+      <div class="hidden sm:flex items-center flex-shrink-0 mr-6">
         <nuxt-link to="/" class="font-semibold text-2xl tracking-tight">John-Robin Tell</nuxt-link>
       </div>
       <div class="flex flex-grow items-center">
         <div class="flex-grow flex justify-end">
-          <nuxt-link to="/projects" class="block px-4 py-2 whitespace-no-wrap">
-            Projects
-          </nuxt-link>
-          <nuxt-link to="/work-history" class="block px-4 py-2 whitespace-no-wrap">
-            Work history
-          </nuxt-link>
-          <nuxt-link to="/proficiencies" class="block px-4 py-2 whitespace-no-wrap">
-            Proficiencies
-          </nuxt-link>
+          <nuxt-link to="/" class="block sm:hidden px-4 py-2 whitespace-no-wrap">Home</nuxt-link>
+          <nuxt-link to="/projects" class="block px-4 py-2 whitespace-no-wrap">Projects</nuxt-link>
+          <nuxt-link to="/work-history" class="block px-4 py-2 whitespace-no-wrap">Work history</nuxt-link>
+          <nuxt-link to="/proficiencies" class="block px-4 py-2 whitespace-no-wrap">Proficiencies</nuxt-link>
           <nuxt-link to="/contact" class="block px-4 py-2 whitespace-no-wrap">Contact me</nuxt-link>
         </div>
       </div>
