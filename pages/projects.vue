f2ce5f08bfed76023a1dfda00df1af30246678be
John <john-robin@jrtell.se>
2020-01-03 15:43:07 +0100

    project update

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,29 +1,33 @@
 <template>
   <div class="container mx-auto pt-48">
-    <div class="project">
-      <div class="project__image" data-aos="fade-right">
-        <img class="w-full" src="https://res.cloudinary.com/jrtell/image/upload/c_scale,f_auto,q_auto,w_300/v1565694490/jrtell/Opera_Snapshot_2019-09-15_174252_localhost">
-      </div>
-      <div class="project__content">
-        <h2 class="project__title" data-aos="fade-up">JRTell.se</h2>
-        <p data-aos="fade-up" data-aos-delay="50">
-          Latest version of my personal site. I rebuild it from scratch every now and then as I get new ideas and become bored of the current design.
-          Built with Nuxt.js and tailwind.css.
-          Statically generated with Nuxt and hosted with Netlify.
-        </p>
-      </div>
-    </div>
+<!--    <div class="project">-->
+<!--      <div class="project__image" data-aos="fade-right">-->
+<!--        <img class="w-full" src="https://res.cloudinary.com/jrtell/image/upload/c_scale,f_auto,q_auto,w_300/v1565694490/jrtell/Opera_Snapshot_2019-09-15_174252_localhost">-->
+<!--      </div>-->
+<!--      <div class="project__content">-->
+<!--        <h2 class="project__title" data-aos="fade-up">JRTell.se</h2>-->
+<!--        <p data-aos="fade-up" data-aos-delay="50">-->
+<!--          Latest version of my personal site. I rebuild it from scratch every now and then as I get new ideas and become bored of the current design.-->
+<!--          Built with Nuxt.js and tailwind.css.-->
+<!--          Statically generated with Nuxt and hosted with Netlify.-->
+<!--        </p>-->
+<!--      </div>-->
+<!--    </div>-->
 
     <div class="project">
       <div class="project__image" data-aos="fade-right">
         <img class="w-full" src="https://res.cloudinary.com/jrtell/image/upload/c_scale,f_auto,q_auto,w_300/v1565695116/jrtell/nordland">
       </div>
       <div class="project__content">
-        <h2 class="project__title" data-aos="fade-up">Nordland - Work In Progress</h2>
+        <h2 class="project__title" data-aos="fade-up">Skaldby - Work In Progress</h2>
         <p data-aos="fade-up" data-aos-delay="50">
-          A personal browser-based online game I've been working on when I haven't felt like working on any other projects.<br>
-          Built as a PWA (Progressive Web Application). Frontend is Nuxt.js with a Sailsjs backend.
+          A personal browser-based online game I've been working on when I haven't felt like working on any other projects. Both for fun and for pushing my skills farther.<br>
+          Built as a PWA (Progressive Web Application) on a microservice architecture.<br>
+          Built around a central API powered by Lumen (PHP framework) with a NuxtJs frontend (VueJs and NodeJs) and a Redis service for caching.
+          Also running one NodeJs service for processing fights and another NodeJs service for pushing notifications and real-time updates to the clients browser.<br>
+          All containerized by Docker.
         </p>
+        <a class="button" href="https://nordland.jrtell.se/" target="_blank" rel="noopener noreferrer" data-aos="fade-up" data-aos-delay="100">Check it out</a>
       </div>
     </div>
 
